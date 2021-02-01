1290fb709acaef1ece32a6a067a12bb4da21df26
ortamfag <a_zhidov@outlook.com>
2021-02-01 18:25:51 +0300

    final

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, url_for, request
+from static.script import randoming
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/', methods=['post', 'get'])
+def add_data():
+    race = request.form.get('select-race')
+    sex = request.form.get('answer')
+    params = randoming(race, sex)
+    del params['sex']
+    del params['race']
+    return render_template('index.html', ready=1, params=params)
+
+
+if __name__ == '__main__':
+    app.run()
